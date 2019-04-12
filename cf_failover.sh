7d5d2a7d198cb8ec6d6649d7f433190376b7880f
dolphycla <dolphy@cloudaccess.net>
2019-04-12 09:06:58 +0100

    Create cf_failover.sh

---
+++ cf_failover.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# cf-failver.sh - https://github.com/dolphycla/scripts
+# A minimal, portable failover client for CloudFlare API v4 meant for use with icinga2
+# Requires: curl (w/ HTTPS support), grep, awk
